808bd083122a1966e1249092cc8dbd895b86039f
sjm <shangjingmin@gmail.com>
2013-06-20 16:36:30 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rappbase::Application.config.secret_token = '1e87b9116f5d5aa97e59ca428647baea9edfe769439ff5ee034b8ab1a39e617ae98fcc117c7a8fcdefa4e89c3b4eaa1507b84b7bb5c33f7f6a81bfe6ac373440'
