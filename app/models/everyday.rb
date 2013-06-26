bc59cb5b9d447dffa99fc6a4c8f5f2ebc6cd6a79
sjm <shangjingmin@gmail.com>
2013-06-26 17:32:50 +0800

    bud everydays business

--- app/models/everyday.rb
+++ app/models/everyday.rb
@@ -1,6 +1,6 @@
 class Everyday < ActiveRecord::Base
   attr_accessible :begin_at, :category, :event, :quantity, :remark, :scene
 
-  CATEGORY = {sleep: [:deep, :light], cry: [:cry], feed: [:feed], medicine: [:calcium, :eczema], excreta: [:weewee, :defecate], wash: [:wash], others: [:others]}
-
+  CATEGORY_EVENTS = {sleep: [:deep, :light], cry: [:cry], feed: [:feed], medicine: [:calcium, :eczema], excreta: [:weewee, :defecate], wash: [:wash], others: [:others]}
+  EVENTS_CATEGORY = Hash[Everyday::CATEGORY_EVENTS.collect{|k,v| v.zip([k]*v.size)}.flatten(1)]
 end
